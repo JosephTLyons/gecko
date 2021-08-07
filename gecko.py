0acf268c7546e1afbe2628268b05096dd63906ff
Joseph T Lyons <JosephTLyons@gmail.com>
2021-08-07 13:43:08 -0400

    A few small adjustments to `disable`

--- gecko.py
+++ gecko.py
@@ -4,9 +4,8 @@ from typing import Callable
 
 def disable(func: Callable) -> Callable:
     """This decorator prevents the function it decorates from executing"""
-
     @wraps(func)
-    def wrapper(*args, **kwargs):  # TODO: type annotations for args
+    def wrapper(*args, **kwargs):  # TODO: type annotations for this function
         print(f"{func.__name__} is disabled")
 
     return wrapper
